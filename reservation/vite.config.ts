48bdbbbecc6baa48c0b05b803cf2a84cf2ed9dc1
Vadym Novitskyi <thrownovitskyi@gmail.com>
2026-01-16 17:19:31 +0200

    refactor name repo in config

--- reservation/vite.config.ts
+++ reservation/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/repository-page/",
+  base: "/Reservation-bot/",
   plugins: [
     vue(),
     vueDevTools(),
